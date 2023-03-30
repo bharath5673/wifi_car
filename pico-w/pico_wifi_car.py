7f90d2dd2299ee979138f31f0b73abd3b86155bd
bharath5673 <s.bharath.2000@gmail.com>
2023-03-30 18:44:17 +0530

    picow/esp32

---
+++ pico-w/pico_wifi_car.py
@@ -0,0 +1,408 @@
+import network
+import socket
+from time import sleep
+import machine
+from machine import Pin, I2C
+import time
+
+
+
+# MicroPython SSD1306 OLED driver, I2C and SPI interfaces
+
+from micropython import const
+import framebuf
+
+
+# register definitions
+SET_CONTRAST = const(0x81)
+SET_ENTIRE_ON = const(0xA4)
+SET_NORM_INV = const(0xA6)
+SET_DISP = const(0xAE)
+SET_MEM_ADDR = const(0x20)
+SET_COL_ADDR = const(0x21)
+SET_PAGE_ADDR = const(0x22)
+SET_DISP_START_LINE = const(0x40)
+SET_SEG_REMAP = const(0xA0)
+SET_MUX_RATIO = const(0xA8)
+SET_COM_OUT_DIR = const(0xC0)
+SET_DISP_OFFSET = const(0xD3)
+SET_COM_PIN_CFG = const(0xDA)
+SET_DISP_CLK_DIV = const(0xD5)
+SET_PRECHARGE = const(0xD9)
+SET_VCOM_DESEL = const(0xDB)
+SET_CHARGE_PUMP = const(0x8D)
+
+# Subclassing FrameBuffer provides support for graphics primitives
+# http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
+class SSD1306(framebuf.FrameBuffer):
+    def __init__(self, width, height, external_vcc):
+        self.width = width
+        self.height = height
+        self.external_vcc = external_vcc
+        self.pages = self.height // 8
+        self.buffer = bytearray(self.pages * self.width)
+        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
+        self.init_display()
+
+    def init_display(self):
+        for cmd in (
+            SET_DISP | 0x00,  # off
+            # address setting
+            SET_MEM_ADDR,
+            0x00,  # horizontal
+            # resolution and layout
+            SET_DISP_START_LINE | 0x00,
+            SET_SEG_REMAP | 0x01,  # column addr 127 mapped to SEG0
+            SET_MUX_RATIO,
+            self.height - 1,
+            SET_COM_OUT_DIR | 0x08,  # scan from COM[N] to COM0
+            SET_DISP_OFFSET,
+            0x00,
+            SET_COM_PIN_CFG,
+            0x02 if self.width > 2 * self.height else 0x12,
+            # timing and driving scheme
+            SET_DISP_CLK_DIV,
+            0x80,
+            SET_PRECHARGE,
+            0x22 if self.external_vcc else 0xF1,
+            SET_VCOM_DESEL,
+            0x30,  # 0.83*Vcc
+            # display
+            SET_CONTRAST,
+            0xFF,  # maximum
+            SET_ENTIRE_ON,  # output follows RAM contents
+            SET_NORM_INV,  # not inverted
+            # charge pump
+            SET_CHARGE_PUMP,
+            0x10 if self.external_vcc else 0x14,
+            SET_DISP | 0x01,
+        ):  # on
+            self.write_cmd(cmd)
+        self.fill(0)
+        self.show()
+
+    def poweroff(self):
+        self.write_cmd(SET_DISP | 0x00)
+
+    def poweron(self):
+        self.write_cmd(SET_DISP | 0x01)
+
+    def contrast(self, contrast):
+        self.write_cmd(SET_CONTRAST)
+        self.write_cmd(contrast)
+
+    def invert(self, invert):
+        self.write_cmd(SET_NORM_INV | (invert & 1))
+
+    def show(self):
+        x0 = 0
+        x1 = self.width - 1
+        if self.width == 64:
+            # displays with width of 64 pixels are shifted by 32
+            x0 += 32
+            x1 += 32
+        self.write_cmd(SET_COL_ADDR)
+        self.write_cmd(x0)
+        self.write_cmd(x1)
+        self.write_cmd(SET_PAGE_ADDR)
+        self.write_cmd(0)
+        self.write_cmd(self.pages - 1)
+        self.write_data(self.buffer)
+
+
+class SSD1306_I2C(SSD1306):
+    def __init__(self, width, height, i2c, addr=0x3C, external_vcc=False):
+        self.i2c = i2c
+        self.addr = addr
+        self.temp = bytearray(2)
+        self.write_list = [b"\x40", None]  # Co=0, D/C#=1
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.temp[0] = 0x80  # Co=1, D/C#=0
+        self.temp[1] = cmd
+        self.i2c.writeto(self.addr, self.temp)
+
+    def write_data(self, buf):
+        self.write_list[1] = buf
+        self.i2c.writevto(self.addr, self.write_list)
+
+
+class SSD1306_SPI(SSD1306):
+    def __init__(self, width, height, spi, dc, res, cs, external_vcc=False):
+        self.rate = 10 * 1024 * 1024
+        dc.init(dc.OUT, value=0)
+        res.init(res.OUT, value=0)
+        cs.init(cs.OUT, value=1)
+        self.spi = spi
+        self.dc = dc
+        self.res = res
+        self.cs = cs
+        import time
+
+        self.res(1)
+        time.sleep_ms(1)
+        self.res(0)
+        time.sleep_ms(10)
+        self.res(1)
+        super().__init__(width, height, external_vcc)
+
+    def write_cmd(self, cmd):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
+
+    def write_data(self, buf):
+        self.spi.init(baudrate=self.rate, polarity=0, phase=0)
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(buf)
+        self.cs(1)
+
+
+# import ssd1306
+i2c_d = I2C(0, sda=Pin(4), scl=Pin(5))
+time.sleep(3)
+display = SSD1306_I2C(128, 64, i2c_d, addr=0x3c)
+display.fill(0)
+display.text('Hello World!', 0, 0, 1)
+display.show()
+
+
+
+# Yes, these could be in another file. But on the Pico! So no more secure. :)
+#ssid = 'STARLINK v2'
+ssid = 'get of my LAN'
+password = 'P@$$w0rD'
+
+def light_onboard_led():
+    led = machine.Pin('LED', machine.Pin.OUT)
+    led.on();
+    time.sleep(0.1)
+    led.off()
+
+# Define pins to pin motors!
+Mot_A_Forward = Pin(18, Pin.OUT)
+Mot_A_Back = Pin(19, Pin.OUT)
+Mot_B_Forward = Pin(20, Pin.OUT)
+Mot_B_Back = Pin(21, Pin.OUT)
+
+def move_forward():
+    Mot_A_Forward.value(1)
+    Mot_B_Forward.value(1)
+    Mot_A_Back.value(0)
+    Mot_B_Back.value(0)
+    print ("Forward")
+    light_onboard_led()
+    
+def move_backward():
+    Mot_A_Forward.value(0)
+    Mot_B_Forward.value(0)
+    Mot_A_Back.value(1)
+    Mot_B_Back.value(1)
+    print ("Backward")
+    light_onboard_led()
+
+def move_stop():
+    Mot_A_Forward.value(0)
+    Mot_B_Forward.value(0)
+    Mot_A_Back.value(0)
+    Mot_B_Back.value(0)
+    print ("Stop")
+    light_onboard_led()
+
+def move_left():
+    Mot_A_Forward.value(1)
+    Mot_B_Forward.value(0)
+    Mot_A_Back.value(0)
+    Mot_B_Back.value(1)
+    print ("Left")
+    light_onboard_led()
+
+def move_right():
+    Mot_A_Forward.value(0)
+    Mot_B_Forward.value(1)
+    Mot_A_Back.value(1)
+    Mot_B_Back.value(0)
+    print ("Right")
+    light_onboard_led()
+
+
+
+
+#Define I2C bus 
+i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1))
+
+#Device address on the I2C bus
+MPU6050_ADDR = 0x68
+
+#PWR_MGMT_1 memory address
+MPU6050_PWR_MGMT_1 = 0x6B
+
+#Accelerometer and Gyroscope's high and low register for each axis
+MPU6050_ACCEL_XOUT_H = 0x3B
+MPU6050_ACCEL_XOUT_L = 0x3C
+MPU6050_ACCEL_YOUT_H = 0x3D
+MPU6050_ACCEL_YOUT_L = 0x3E
+MPU6050_ACCEL_ZOUT_H = 0x3F
+MPU6050_ACCEL_ZOUT_L = 0x40
+MPU6050_GYRO_XOUT_H = 0x43
+MPU6050_GYRO_XOUT_L = 0x44
+MPU6050_GYRO_YOUT_H = 0x45
+MPU6050_GYRO_YOUT_L = 0x46
+MPU6050_GYRO_ZOUT_H = 0x47
+MPU6050_GYRO_ZOUT_L = 0x48
+
+#Accelerometer's LSB/g (least significant bits per gravitational force) sensitivity
+MPU6050_LSBG = 16384.0
+
+#Gyroscope's LSB/g sensitivity
+MPU6050_LSBDS = 131.0 
+
+
+#Set all bits in the PWR_MGMT_1 register to 0
+def mpu6050_init(i2c):
+    i2c.writeto_mem(MPU6050_ADDR, MPU6050_PWR_MGMT_1, bytes([0]))
+
+
+def combine_register_values(h, l):
+    if not h[0] & 0x80:
+        return h[0] << 8 | l[0]
+    return -((h[0] ^ 255) << 8) |  (l[0] ^ 255) + 1
+
+
+#Get Accelerometer values
+def mpu6050_get_accel(i2c):
+    accel_x_h = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_ACCEL_XOUT_H, 1)
+    accel_x_l = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_ACCEL_XOUT_L, 1)
+    accel_y_h = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_ACCEL_YOUT_H, 1)
+    accel_y_l = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_ACCEL_YOUT_L, 1)
+    accel_z_h = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_ACCEL_ZOUT_H, 1)
+    accel_z_l = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_ACCEL_ZOUT_L, 1)
+    
+    return [combine_register_values(accel_x_h, accel_x_l) / MPU6050_LSBG,
+            combine_register_values(accel_y_h, accel_y_l) / MPU6050_LSBG,
+            combine_register_values(accel_z_h, accel_z_l) / MPU6050_LSBG]
+
+
+#Get Gyroscope values
+def mpu6050_get_gyro(i2c):
+    gyro_x_h = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_GYRO_XOUT_H, 1)
+    gyro_x_l = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_GYRO_XOUT_L, 1)
+    gyro_y_h = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_GYRO_YOUT_H, 1)
+    gyro_y_l = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_GYRO_YOUT_L, 1)
+    gyro_z_h = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_GYRO_ZOUT_H, 1)
+    gyro_z_l = i2c.readfrom_mem(MPU6050_ADDR, MPU6050_GYRO_ZOUT_L, 1)
+    
+    return [combine_register_values(gyro_x_h, gyro_x_l) / MPU6050_LSBDS,
+            combine_register_values(gyro_y_h, gyro_y_l) / MPU6050_LSBDS,
+            combine_register_values(gyro_z_h, gyro_z_l) / MPU6050_LSBDS]
+
+
+
+
+
+
+#Stop the robot as soon as possible
+move_stop()
+    
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    light_onboard_led()
+    return ip
+    
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+
+def webpage(accelerometer_values, gyrometer_values):
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <title>PICO-W Robot Control</title>
+            </head>
+
+            <body>
+            <center><b>
+            <form action="./forward">
+            <input type="submit" value="Forward" style="height:120px; width:120px" />
+            </form>
+            <table><tr>
+            <td><form action="./left">
+            <input type="submit" value="Left" style="height:120px; width:120px" />
+            </form></td>
+            <td><form action="./stop">
+            <input type="submit" value="Stop" style="height:120px; width:120px" />
+            </form></td>
+            <td><form action="./right">
+            <input type="submit" value="Right" style="height:120px; width:120px" />
+            </form></td>
+            </tr></table>
+            <form action="./back">
+            <input type="submit" value="Back" style="height:120px; width:120px" />
+            </form>
+
+            <p>Accelerometer: <span id="accelerometerValues">{accelerometer_values}</span></p>
+            <p>Gyroscope: <span id="gyrometerValues">{gyrometer_values}</span></p>
+
+            </body>
+            </html>
+            """
+    return str(html)
+
+def serve(connection):
+    i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1))
+    mpu6050_init(i2c)
+    #Start web server
+    while True:
+
+        print("Accelerometer:\t", mpu6050_get_accel(i2c), "g") #Print Accelerometer values (X,Y,Z) 
+        print("Gyroscope:\t", mpu6050_get_gyro(i2c), "°/s") #Print Gyroscope values (X,Y,Z)
+
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        if request == '/forward?':
+            move_forward()
+        elif request =='/left?':
+            move_left()
+        elif request =='/stop?':
+            move_stop()
+        elif request =='/right?':
+            move_right()
+        elif request =='/back?':
+            move_backward()
+        html = webpage(accelerometer_values=mpu6050_get_accel(i2c), gyrometer_values=mpu6050_get_gyro(i2c))
+        client.send(html)
+        client.close()
+
+try:
+    ip = connect()
+    connection = open_socket(ip)
+    serve(connection)
+except KeyboardInterrupt:
+    machine.reset()
+
+    
